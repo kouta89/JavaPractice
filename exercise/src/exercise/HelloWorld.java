24bf8bf22e89c5e3e382e0eb13e60f10669a9ce0
busam <busam@DESKTOP-I300IL4>
2023-05-10 09:54:35 +0900

    [add]HelloWorld

---
+++ exercise/src/exercise/HelloWorld.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println(22+23);
+		int x;
+		x=6;
+		System.out.println(x*x);
+	}
+}
