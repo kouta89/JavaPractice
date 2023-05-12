b3b0934c1b9522b20d618a818124f285f13f0adc
kouta89 <yamamotojb89@gmail.com>
2023-05-12 09:29:06 +0900

    [add]practice1,2

---
+++ exercise/src/exercise/Practice1.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Practice1{
+	public static void main(String[] args) {
+		int height = 3;
+		int width = 5;
+		System.out.println("縦幅"+height+"横幅"+width
+				+"の長方形の面積は、"+height*width);
+	}
+}
---
+++ exercise/src/exercise/Practice2.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Practice2{
+	public static void main(String[] args) {
+		boolean hit = true;
+		char attackType = '駆'; 
+		double magnification = 3.14;
+		long number = 314159265853979L;
+		String attackText = "ナエギの攻撃!";
+	}
+}
