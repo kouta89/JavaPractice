488ad4c488a23eed996d5a044bc8a8aefd7b41f2
kouta89 <yamamotojb89@gmail.com>
2023-05-18 12:54:25 +0900

    [add]MathPractice

---
+++ exercise/src/exercise/MathPractice.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class MathPractice{
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int m = Math.max(a, b);
+		System.out.println("比較実験:"+a+"と"+b+"とで大きい方は"+m);
+	}
+}
