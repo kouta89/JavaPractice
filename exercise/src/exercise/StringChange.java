111a068e977bf4973561752b35a8ba28e8ca24d7
kouta89 <yamamotojb89@gmail.com>
2023-05-19 09:56:49 +0900

    [add]stringChange

---
+++ exercise/src/exercise/StringChange.java
@@ -0,0 +1,9 @@
+package exercise;
+
+public class StringChange{
+	public static void main(String[] args) {
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは来年、"+(n+1)+"さい");
+	}
+}
