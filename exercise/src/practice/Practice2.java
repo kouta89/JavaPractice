17d74f0990ba8a876ae943a4df447dc78b5b5baa
kouta89 <yamamotojb89@gmail.com>
2023-05-24 09:51:06 +0900

    [add]Practice2

---
+++ exercise/src/practice/Practice2.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Practice2{
+	public static void main(String[] args) {
+		int seibetsu = 1;
+		
+		int age = 22;
+		
+		System.out.println("こんにちは");
+		
+		if(seibetsu == 0) {
+			System.out.println("男");
+		}else {
+			System.out.println("女");
+			System.out.println(age+"さい");
+			System.out.println("よろしく");
+		}
+	}
+}
