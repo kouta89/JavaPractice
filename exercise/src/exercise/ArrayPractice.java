09a882e4a9b73e1a9f5cc9b263a47b172f4b8622
kouta89 <yamamotojb89@gmail.com>
2023-05-28 10:09:07 +0900

    [add]arrayPractice

---
+++ exercise/src/exercise/ArrayPractice.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class ArrayPractice{
+	public static void main(String[] args){
+		
+		int[] score = new int[5];
+		score[1] = 30;
+		System.out.println(score[1]);
+	}
+}
