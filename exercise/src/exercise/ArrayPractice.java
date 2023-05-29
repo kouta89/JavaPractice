7bf2e4544cedc49d75059f785830db1fcc170294
kouta89 <yamamotojb89@gmail.com>
2023-05-29 10:01:59 +0900

    [add]arrayPractice

--- exercise/src/exercise/ArrayPractice.java
+++ exercise/src/exercise/ArrayPractice.java
@@ -3,8 +3,10 @@ package exercise;
 public class ArrayPractice{
 	public static void main(String[] args){
 		
-		int[] score = new int[5];
-		score[1] = 30;
-		System.out.println(score[1]);
+		int[] score = new int[] {20,30,40,50,80};
+		score[2] += 20;
+		for(int value : score) {
+			System.out.println(value);
+		}
 	}
 }
